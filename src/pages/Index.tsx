69c192b253aac52922e71260238a90149cdd0576
samarth <92245868+sammyZi@users.noreply.github.com>
2025-07-30 13:56:08 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -501,7 +501,7 @@ const Index = () => {
                       <a
                         href={detail.href}
                         target="_blank"
-                        className="text-muted-foreground hover:underline transition-colors glow-text"
+                        className="text-muted-foreground hover:underline transition-colors"
                       >
                         {detail.value}
                       </a>
