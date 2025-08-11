03ffcd0ffe0ae3b7214fd75e4bbcae2716737622
samarth <92245868+sammyZi@users.noreply.github.com>
2025-08-11 21:05:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
