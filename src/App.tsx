36ba04cff9d847012d86b7d42a32cce4b6b1c7fa
samarth <92245868+sammyZi@users.noreply.github.com>
2025-08-16 11:53:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => (
   <TooltipProvider>
