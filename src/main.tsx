103a45333abe5ede93c9336d609bb207de15cd5a
samarth <92245868+sammyZi@users.noreply.github.com>
2025-08-16 11:54:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./providers/ThemeProvider";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient();
 
